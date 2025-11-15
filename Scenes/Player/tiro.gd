ac998efee71cbb1936b70f4e415ddb93b0bbc287
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-11-15 17:30:41 -0300

    faz a area no final de cada nivel

--- Scenes/Player/tiro.gd
+++ Scenes/Player/tiro.gd
@@ -6,8 +6,10 @@ const speed: float = 800
 func _process(delta: float) -> void:
 	position.y -= speed * delta
 
-func _on_body_entered(_body: Node2D) -> void:
-	queue_free()
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("atiravel"):
+		body.queue_free()  # Destroi o objeto
+		queue_free()       # Destroi o tiro
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
